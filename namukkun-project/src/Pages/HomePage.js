51440fe14858a7c992dcaa7eae8d0b55a8fea9e3
LYH513 <peperony367@gmail.com>
2024-06-24 22:03:05 +0900

    Feat: themeProvider 기능을 통한 스타일 공통 관리 기능 추가

--- namukkun-project/src/Pages/HomePage.js
+++ namukkun-project/src/Pages/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import Home from "../Components/Home_Components/Home";
 
 //Home_Components의 컴포넌트들을 여기 불러오면 됨. 화면에 띄울 페이지
 function HomePage(){
 
   return(
-    <div>첫번째 화면</div>
+    <div>
+      <Home/>
+    </div>
   );
 }
 export default HomePage;
