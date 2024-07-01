7181a73300b9509d1b745dc9aff8a1bf1140d58c
LYH513 <peperony367@gmail.com>
2024-07-01 17:17:23 +0900

    Design: 제안게시판 배너 추가

--- namukkun-project/src/API/AxiosAPI.js
+++ namukkun-project/src/API/AxiosAPI.js
@@ -6,6 +6,11 @@ const server = process.env.REACT_APP_SERVER;
 export const getSendCodeAPI = async (code) => {
   try {
     const response = await axios.get(`${server}?code=${code}`,{withCredentials: true,});
+
+    // 쿠키 읽기
+    // const allCookies = document.cookie;
+    // console.log('All Cookies:', allCookies);
+
     return response;
   } catch (err) {
     console.error(err);
