62359b6f5098e21987781b9caedec10bbcb3fb9c
LYH513 <peperony367@gmail.com>
2024-07-02 14:57:18 +0900

    Feat: 카카오 로그인 서버 연결 완료

--- namukkun-project/src/API/AxiosAPI.js
+++ namukkun-project/src/API/AxiosAPI.js
@@ -7,13 +7,10 @@ const server = process.env.REACT_APP_SERVER;
 export const getSendCodeAPI = async (code) => {
   try {
     const response = await axios.get(`${server}?code=${code}`,{withCredentials: true,});
-
-    // 쿠키 읽기
-    // const allCookies = document.cookie;
-    // console.log('All Cookies:', allCookies);
-
     return response;
   } catch (err) {
     console.error(err);
+    console.log('1차');
+    throw err ;
   }
 };
--- namukkun-project/src/Components/Login_Components/KaKaoLogin.js
+++ namukkun-project/src/Components/Login_Components/KaKaoLogin.js
@@ -1,24 +1,39 @@
-import React, { useEffect } from "react";
-import { Link, NavLink, useParams } from "react-router-dom";
-import styled from "styled-components";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, NavLink, useParams , useNavigate} from "react-router-dom";
 import { getSendCodeAPI } from "../../API/AxiosAPI";
 
 //카카오 로그인을 위해 카카오 로그인 페이지에서 인가코드를 추출해 서버에 보내는 함수
 function KaKaoLogin(){
+  const navigate = useNavigate();
+  let state =1;
 
   const params = new URL(document.location.toString()).searchParams;
-
   const code = params.get('code');
-  if (code) {
-   const response = getSendCodeAPI(code);
-   console.log(response);
-  }
 
+    const sendCode = async (code) => {
+      try {
+        const response = await getSendCodeAPI(code);
+        console.log("성공",response);
+        navigate('/');  // (로그인)성공했을 때 이동할 경로
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          navigate('/list');  // 401 에러(회원가입해야함)일 때 이동할 경로
+        } else {
+         console.error("에러", err);
+        }
+      }
+    };
+
+  useEffect(()=>{
+    if (code && (state===1)) {
+      sendCode(code);
+      ++state;
+    }
+  },[])
   //아래에 스피너 등의 화면이 보여야 할 것 같음
   return(
     <div >
-      로그인~~
+      로그인 중~~
     </div>
   );
 }
