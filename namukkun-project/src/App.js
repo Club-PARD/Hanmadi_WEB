d1478f767101161daa7f521b3e0b535c3b397f1f
이온유 <ion-yu@ion-yuui-MacBookPro.local>
2024-07-03 22:55:55 +0900

    Design: mypage 완성

--- namukkun-project/src/App.js
+++ namukkun-project/src/App.js
@@ -5,6 +5,7 @@ import MainPage from './Pages/MainPage';
 import KaKaoLogin from './Components/Login_Components/KaKaoLogin';
 import ListPage from './Pages/ListPage';
 import SelectRegionPage from './Pages/SelectRegionPage';
+import MyPage from './Pages/MyPage';
 
 //Pages 폴더로부터 컴포넌트를 가져와서 라우터 연결  
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path='/login/oauth2/code/kakao' element={<KaKaoLogin/>}/>
       <Route path='/list' element={<ListPage/>}/>
       <Route path='/selectregion' element={<SelectRegionPage/>}/>
+      <Route path='/mypage' element={<MyPage/>}/>
     </Routes>
 
   );
