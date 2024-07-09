935f0a59898c647fed6143ed1aa9b5452c16d4ae
이온유 <ion-yu@ion-yuui-MacBookPro.local>
2024-07-09 19:01:34 +0900

    Feat: 수정 삭제 클백 연결 상세페이지

--- namukkun-project/src/Components/Postit_Components/AdviseDelete.js
+++ namukkun-project/src/Components/Postit_Components/AdviseDelete.js
@@ -1,18 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { GlobalStyle } from '../../Assets/Style/theme';
+import DeleteModal from "./DeleteModal";
+import { useParams, useNavigate } from 'react-router-dom';
 
 function AdviseDelete() {
+    const { postId } = useParams(); // URL에서 postId를 가져옴
+    const navigate = useNavigate(); // useNavigate 훅 사용
+    const [isWModalOpen, setIsWModalOpen] = useState(false);
+    const [update, setUpdate] = useState(false);
 
+    const handleWModalOpen = () => {
+        setIsWModalOpen(true);
+    };
+
+    const handleWModalClose = () => {
+        setIsWModalOpen(false);
+    };
+
+    const handleModifyClick = () => {
+        navigate(`/modify/${postId}`); // 수정 페이지로 이동
+    };
 
     return (
-    <Div>
-        <GlobalStyle/>
+        <Div>
+            <GlobalStyle />
             <AdvDelBtuContainer>
-                <AdviseButton>수정</AdviseButton>
-                <DeleteButton>삭제</DeleteButton>
+                <AdviseButton onClick={handleModifyClick}>수정</AdviseButton>
+                <DeleteButton onClick={handleWModalOpen}>삭제</DeleteButton>
             </AdvDelBtuContainer>
-    </Div>
+            <DeleteModal 
+                isOpen={isWModalOpen} 
+                closeModal={handleWModalClose} 
+                postId={postId} 
+                setUpdate={setUpdate} 
+                update={update} 
+            />
+        </Div>
     );
 }
 
@@ -25,16 +49,20 @@ const Div = styled.div`
 const AdvDelBtuContainer = styled.div`
     width: 100%;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
+    padding-left: 91px;
+    gap: 10px;
     flex-direction: row;
     align-items: center;
     height: 30px;
-`
+    padding-top: 65px;
+    padding-bottom: 22px;
+`;
 
 const AdviseButton = styled.button`
     display: flex;
-    width: 38px;
-    height: 20px;
+    width: 63px;
+    height: 33px;
     padding: 5.849px;
     justify-content: center;
     align-items: center;
@@ -45,7 +73,7 @@ const AdviseButton = styled.button`
     color: var(--gray-005, #707070);
     text-align: center;
     font-family: 'MinSans-Regular';
-    font-size: 10px;
+    font-size: 13px;
     font-style: normal;
     font-weight: 500;
     cursor: pointer;
@@ -54,8 +82,8 @@ const AdviseButton = styled.button`
 
 const DeleteButton = styled.button`
     display: flex;
-    width: 38px;
-    height: 20px;
+    width: 63px;
+    height: 33px;
     padding: 5.849px;
     justify-content: center;
     align-items: center;
@@ -66,7 +94,7 @@ const DeleteButton = styled.button`
     color: var(--Main-001, #005AFF);
     text-align: center;
     font-family: 'MinSans-Regular';
-    font-size: 10px;
+    font-size: 13px;
     font-style: normal;
     font-weight: 500;
     cursor: pointer;
